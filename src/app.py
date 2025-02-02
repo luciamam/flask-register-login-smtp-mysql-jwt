616138ef832c69663da45cb919cf3478fd8d2a63
luciamam <lumanmba2023@gmail.com>
2025-02-02 13:44:11 +0100

    redireccionando de la ruta / a la ruta /login

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,redirect,url_for
 
 
 
@@ -9,7 +9,10 @@ app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Soy la ruta raiz que le va servir el formulario de login "
+    #cuando aterrizemos a la ruta raiz debemos redirigirnos a la ruta login 
+    #return "Soy la ruta raiz que le va servir el formulario de login "
+
+    return redirect(url_for('login'))
 
 @app.route('/register')
 def register():
